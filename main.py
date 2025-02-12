787405ddced5cd116a9404a2b4a2f446738cdaac
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-13 00:55:33 +0500

    perform one hot encoding on the data set

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ from keras.datasets import mnist
 print(xtrain.shape)
 plt.imshow(xtrain[0])
 plt.savefig('./pngFiles/data_1.png')
+'''Conventional Neural Network we need to flatten the images into one-dimensional vectors '''
+num_of_pixels = xtrain.shape[1]*xtrain.shape[2]
+xtrain = xtrain.reshape(xtrain.shape[0],num_of_pixels).astype('float32')
+xtest = xtest.reshape(xtest.shape[0],num_of_pixels).astype('float32')
+print(xtrain.shape)
+print(xtest.shape)
+xtrain=xtrain/255
+xtest=xtest/255
+ytrain=to_categorical(ytrain)
+ytest=to_categorical(ytest)
+num_of_classes = ytest.shape[1]
+print(num_of_classes)
+print(num_of_pixels)
+def classification_model():
+    model=Sequential()
+    model.add(input(shape=num_of_pixels))
